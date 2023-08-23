979f197068bbd78d1251ecfec4460ba206f6b67c
MARKLUONG1 <4106948-MARKLUONG1@users.noreply.replit.com>
2023-08-23 22:03:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+function L() {
+  console.log("LLL");
+  console.log("LLL");
+  console.log("LLL");
+  console.log("LLL");
+  console.log("LLL");
+  console.log("LLL");
+  console.log("LLL")
+  console.log("LLLLLLLLLLL");
+  console.log("LLLLLLLLLLL");
+  console.log(" ");
+}
+
+function H() {
+  console.log("HHH     HHH");
+  console.log("HHH     HHH");
+  console.log("HHH     HHH");
+  console.log("HHHHHHHHHHH");
+  console.log("HHHHHHHHHHH");
+  console.log("HHH     HHH");
+  console.log("HHH     HHH");
+  console.log("HHH     HHH");
+  console.log(" ");
+}
+
+function S() {
+  console.log("SSSSSSSSSSS");
+  console.log("SSS        ");
+  console.log("SSS        ");
+  console.log("SSS        ");
+  console.log("SSSSSSSSSSS");
+  console.log("        SSS");
+  console.log("        SSS");
+  console.log("        SSS");
+  console.log("SSSSSSSSSSS");
+  console.log(" ");
+}
+
+for (var i=0; i < 200; i++) {
+  L();
+  H();
+  S();
+}
